a2f8b352a13955de28c2e22da719e3cbc65a3b3c
Kumbus <72298393+Kumbus@users.noreply.github.com>
2022-04-13 22:42:49 +0200

    Add files via upload

---
+++ ValueConverters/ApplicationPageValueConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Projekt
+{
+    /// <summary>
+    /// Converts the <see cref="ApplicationPage"/> to an actual view/page
+    /// </summary>
+    public class ApplicationPageValueConverter : BaseValueConverter<ApplicationPageValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Find the appropriate page
+            switch ((ApplicationPage)value)
+            {
+                case ApplicationPage.Main:
+                    return new MainPage();
+
+                case ApplicationPage.Login:
+                    return new LoginPage();
+
+                case ApplicationPage.Registration:
+                    return new RegistrationPage();
+
+                case ApplicationPage.Account:
+                    return new AccountPage();
+
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
