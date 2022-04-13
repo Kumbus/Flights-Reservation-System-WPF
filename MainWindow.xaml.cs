4b6aea6a54f6171cb618f250cdde5ee22e7df258
Kumbus <72298393+Kumbus@users.noreply.github.com>
2022-04-13 22:30:19 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace Projekt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = WindowViewModel.GetInstanceWindowViewModel();
+        }
+
+
+        private void Minimise(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized; 
+        }
+
+        private void ChangeSize(object sender, RoutedEventArgs e)
+        {
+            if(WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else
+                WindowState = WindowState.Maximized;
+        }
+
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            WindowViewModel windowViewModel = WindowViewModel.GetInstanceWindowViewModel();
+            windowViewModel.CreateCloseLog();
+            Close();
+        }
+
+
+
+    }
+}
